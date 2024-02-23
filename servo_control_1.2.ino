f42f697eb8bc19e11181f303e9145c027df19c0b
kenneth11112 <124687232+kenneth11112@users.noreply.github.com>
2024-02-23 10:46:51 +0800

    Update servo_control_1.2.ino

--- servo_control_1.2.ino
+++ servo_control_1.2.ino
@@ -10,11 +10,11 @@ const int bt = 4;   // connect button to pin 3
 volatile byte state = LOW;
 
 //Define variables
-int sw1State = 0;
-int sw2State = 0;
-int btState = 0;
+volatile byte sw1State = LOW;
+volatile byte sw2State = LOW;
+volatile byte btState = LOW;
 int pos;        // variable to store the servo position
-int time = 0.018;  // time between each step
+int time = 60;  // time between each step
 
 void setup() {
   Serial.begin(9600);
@@ -23,27 +23,30 @@ void setup() {
 
   pinMode(sw1, INPUT);
   pinMode(sw2, INPUT);
-  pinMode(bt, INPUT);
   pinMode(bt, INPUT_PULLUP);
   attachInterrupt(digitalPinToInterrupt(bt), stop, CHANGE);
 }
 
 void loop() {
   while (state == LOW) {
-    if (sw1 == HIGH) {
-      for (pos = 0; pos <= 180; pos += 1) {  // goes from 0 degrees to 180 degrees
+    if (sw1State == HIGH) {
+      for (pos >= 0; pos <= 90; pos += 1) {  	// goes from 0 degrees to 90 degrees
         // in steps of 1 degree
-        m1.write(pos);  // tell servo to go to position in variable 'pos'
+        m1.write(pos);  											// tell servo to go to position in variable 'pos'
         Serial.println(pos);
-        delay(time);  // default as waiting 15ms for the servo to reach the position
+        delay(time);  												// default as waiting 15ms for the servo to reach the position
       }
     }
 
-    if (sw2 == HIGH) {
-      for (pos = 180; pos >= 0; pos -= 1) {  // goes from 180 degrees to 0 degrees
-        m1.write(pos);                  // tell servo to go to position in variable 'pos'
-        delay(15);                           // waits 15ms for the servo to reach the position
+    if (sw2State == HIGH) {
+      for (pos <= 90; pos >= 0; pos -= 1) {  	// goes from 90 degrees to 0 degrees
+        m1.write(pos);                  			// tell servo to go to position in variable 'pos'
+				Serial.println(pos);
+        delay(15);                           	// waits 15ms for the servo to reach the position
       }
+		}
+		if (btState == HIGH){
+			stop();
     }
   }
 }
